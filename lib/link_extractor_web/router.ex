7bf940aa501d569c9c6499156e1627d2bd558024
Josh Adams <josh@isotope11.com>
2014-07-19 15:52:02 -0500

    Fix up the views a little bit (no phoenix boilerplate)

--- lib/link_extractor_web/router.ex
+++ lib/link_extractor_web/router.ex
@@ -2,7 +2,7 @@ defmodule LinkExtractorWeb.Router do
   use Phoenix.Router
 
   plug Plug.Static, at: "/static", from: :link_extractor_web
-  get "/", LinkExtractorWeb.Controllers.Pages, :index, as: :page
+  get "/", LinkExtractorWeb.Controllers.Links, :index
   get "/links", LinkExtractorWeb.Controllers.Links, :index
   post "/messages", LinkExtractorWeb.Controllers.Messages, :create
 end
